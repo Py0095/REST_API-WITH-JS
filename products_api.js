3ae9de5a23ec54bcdebb596d674163250e75f290
Aliano Charles <95935774+Py0095@users.noreply.github.com>
2023-01-31 15:44:01 -0500

    Add files via upload

--- products_api.js
+++ products_api.js
@@ -4,15 +4,19 @@ import all_products from "./producs.js";
 
 const app = express();
 app.use((req, res, next) => {
-    res.set("Access-Control-Allowed", "*");
+    const origin = req.headers.origin
+    if (origin === 'localhost:8000' || origin ==='localhost:9007') {
+        res.set("Access-Control-Allowed",origin);  
+    }
+   
     res.set(
         "Access-Control-Methods",
         "GET",
-        "POST",
-        "OPTIONS",
-        "PUT",
-        "PATCH",
-        "DELETE"
+        // "POST",
+        // "OPTIONS",
+        // "PUT",
+        // "PATCH",
+        // "DELETE"
     );
     next();
 });
@@ -21,17 +25,17 @@ app.get("/", (req, res) => {
     return res.send("Welcome to my products api!");
 });
 
-app.get("/products",Authentification, (req, res) => {
+app.get("/products", (req, res) => {
     return res.send(200, all_products);
 });
 
-app.get("/products/:id",Authentification, (req, res) => {
+app.get("/products/:id", (req, res) => {
     const id = parseInt(req.params.id);
     const products_id = all_products.filter((product) => product.id === id);
     return res.send(200, products_id);
 });
 
-app.get("/products/brands", Authentification, (req, res) => {
+app.get("/products/brands", (req, res) => {
     const products_brands = all_products.map((el) => el.brand);
     res.send(200, products_brands);
 });
