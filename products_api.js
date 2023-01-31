f7cbb05e2047bd70411ae74c6e39ac50d94d7589
Aliano Charles <95935774+Py0095@users.noreply.github.com>
2023-01-31 04:20:53 -0500

    Add files via upload
    second

--- products_api.js
+++ products_api.js
@@ -1,32 +1,27 @@
-import express from 'express'
-// import Authentification from "./authentification.js";
+import express from "express";
+import Authentification from "./authentification.js";
 import all_products from "./producs.js";
 
 const app = express();
-
 
 app.get("/", (req, res) => {
     return res.send("Welcome to my products api!");
 });
 
-app.get("/products", (req, res) => {
-    return res.send(200,all_products);
+app.get("/products",Authentification, (req, res) => {
+    return res.send(200, all_products);
 });
 
-app.get('/products/:id', (req, res) => {
+app.get("/products/:id",Authentification, (req, res) => {
     const id = parseInt(req.params.id);
-    const products_id = all_products.filter(product => product.id === id);
+    const products_id = all_products.filter((product) => product.id === id);
     return res.send(200, products_id);
 });
 
-app.get('/products/brands',(req, res) => {
-    return res.send('brands products')
-})
-// app.get("/products/brands",(req, res) => {
-//   const products_brands =  all_products.forEach(el => {
-//     element.brand
-// });
-//     return res.send(400);
-// });
-
-app.listen(3001);
+app.get("/products/brands",Authentification,(req, res) => {
+    const products_brands = all_products.forEach(el=>el.brand)
+});
+const port = 3001;
+app.listen(() => {
+    console.log(`Listening on port: ${port}`);
+});
