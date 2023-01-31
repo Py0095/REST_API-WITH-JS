136c4b65c4aeb92a0f5323d09fb7272127e86529
Aliano Charles <95935774+Py0095@users.noreply.github.com>
2023-01-31 16:47:53 -0500

    Add files via upload

--- products_api.js
+++ products_api.js
@@ -5,10 +5,11 @@ import all_products from "./producs.js";
 const app = express();
 app.use((req, res, next) => {
     const origin = req.headers.origin
-    if (origin === 'localhost:8000' || origin ==='localhost:9007') {
-        res.set("Access-Control-Allowed",origin);  
-    }
-   
+    if (origin === 'http://localhost:8000' || origin ==='http://localhost:9007')
+        res.set("Access-Control-Allow-Origin",origin); 
+    else
+        return send(401)
+
     res.set(
         "Access-Control-Methods",
         "GET",
@@ -25,17 +26,17 @@ app.get("/", (req, res) => {
     return res.send("Welcome to my products api!");
 });
 
-app.get("/products", (req, res) => {
+app.get("/products",Authentification, (req, res) => {
     return res.send(200, all_products);
 });
 
-app.get("/products/:id", (req, res) => {
+app.get("/products/:id",Authentification, (req, res) => {
     const id = parseInt(req.params.id);
     const products_id = all_products.filter((product) => product.id === id);
     return res.send(200, products_id);
 });
 
-app.get("/products/brands", (req, res) => {
+app.get("/products/brands",Authentification, (req, res) => {
     const products_brands = all_products.map((el) => el.brand);
     res.send(200, products_brands);
 });
